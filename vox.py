365901a5052cf089d5ebf8a7d007475123601edb
sulfierry <leon.bio0@hotmail.com>
2023-09-11 18:53:51 -0300

    vox

--- vox.py
+++ vox.py
@@ -1,11 +1,9 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors
 import matplotlib.patches as patches
 
 class PDBVoxel:
-
-        # Classificação dos aminoácidos
     PROPERTIES = {
         'HIDROPHOBIC': ['ALA', 'ILE', 'LEU', 'MET', 'PHE', 'PRO', 'TRP', 'VAL'],
         'POSITIVE': ['ARG', 'HIS', 'LYS'],
@@ -13,7 +11,6 @@ class PDBVoxel:
         'NEGATIVE': ['ASP', 'GLU']
     }
 
-    # Paleta de cores com base nas propriedades
     COLOR_PALETTE = {
         'HIDROPHOBIC': "#FFFF00",
         'POSITIVE': "#0000FF",
@@ -27,7 +24,6 @@ class PDBVoxel:
         self.coord = coord
         self.center = center
         self.grid_size = grid_size
-
 
     def plot_voxel(self):
         fig = plt.figure()
@@ -66,21 +62,18 @@ class PDBVoxel:
                 }
                 parsed_data['chains'].append(parsed_atom)
             elif line.startswith(("HETATM", "ANISOU", "TER", "CONECT")):
-                # Processa outras linhas conforme necessário
-                pass  # Placeholder para processamento adicional
+                pass
 
         return parsed_data
     
     def coord_to_voxel(self, coord):
         voxel_coord = np.floor((coord - self.center) / self.grid_size).astype(int)
         if np.any(voxel_coord < 0) or np.any(voxel_coord >= self.voxel_grid.shape):
-            print(f"Invalid voxel coordinates: {voxel_coord} for atom coordinate: {self.coord}")
+            print(f"Invalid voxel coordinates: {voxel_coord} for atom coordinate: {coord}")
             return None
         return voxel_coord
 
-
     def pdb_to_voxel_atom(self, parsed_pdb):
-        global atom_info_grid
         atom_info_grid = np.empty(self.voxel_grid.shape, dtype=object)
         
         for atom_section in ["chains", "cofactors", "ligands"]:
@@ -116,37 +109,79 @@ class PDBVoxel:
 
         return self.voxel_grid, residue_info_grid
 
-    
     def project_sum(self, axis=2):
-        """
-            Projeção de Soma:
-            Esta abordagem soma os valores ao longo de uma dimensão. 
-            É útil quando você está interessado em saber quantos átomos ou aminoácidos existem ao longo dessa dimensão.
-        """
         if axis not in [0, 1, 2]:
             raise ValueError("Invalid axis. Choose from 0, 1, or 2.")
         return np.sum(self.voxel_grid, axis=axis)
 
 
+# Functions outside the class
+def get_amino_acid_property(res_name):
+    for property, amino_acids in PDBVoxel.PROPERTIES.items():
+        if res_name in amino_acids:
+            return property
+    return None
 
 
-# Definição do tamanho e origem da grade
-grid_dim = [10, 10, 10]
+def pdb_to_voxel_property(parsed_pdb, voxel_instance):
+    property_grid = np.empty(voxel_instance.voxel_grid.shape, dtype=object)
+    for atom_section in ["chains", "cofactors", "ligands"]:
+        for atom_details in parsed_pdb[atom_section]:
+            voxel_coord = voxel_instance.coord_to_voxel(np.array(atom_details["coord"]))
+            if voxel_coord is not None:
+                aa_property = get_amino_acid_property(atom_details['res_name'])
+                if aa_property:
+                    property_grid[tuple(voxel_coord)] = aa_property
+    return property_grid
+
+
+def project_sum_with_property(voxel_instance, property_grid, axis=2):
+    projection_sum = np.sum(voxel_instance.voxel_grid, axis=axis)
+    property_projection = np.empty(projection_sum.shape, dtype=object)
+    for x in range(projection_sum.shape[0]):
+        for y in range(projection_sum.shape[1]):
+            if axis == 0:
+                properties_in_column = property_grid[x, y, :]
+            elif axis == 1:
+                properties_in_column = property_grid[x, :, y]
+            else:
+                properties_in_column = property_grid[:, x, y]
+            properties_in_column = [prop for prop in properties_in_column if prop]
+            if properties_in_column:
+                unique, counts = np.unique(properties_in_column, return_counts=True)
+                predominant_property = unique[np.argmax(counts)]
+                property_projection[x, y] = predominant_property
+    return projection_sum, property_projection
+
+
+def plot_projection_with_property(projection_sum, property_projection):
+    fig, ax = plt.subplots(figsize=(10, 10))
+    colored_projection = np.zeros(projection_sum.shape + (3,))
+    for x in range(projection_sum.shape[0]):
+        for y in range(projection_sum.shape[1]):
+            if property_projection[x, y]:
+                color = matplotlib.colors.hex2color(PDBVoxel.COLOR_PALETTE[property_projection[x, y]])
+                intensity = projection_sum[x, y] / projection_sum.max()
+                colored_projection[x, y] = [c * intensity for c in color]
+    ax.imshow(colored_projection)
+    ax.set_title('Sum Projection with Amino Acid Properties')
+    patches_list = [patches.Patch(color=PDBVoxel.COLOR_PALETTE[prop], label=prop) for prop in PDBVoxel.COLOR_PALETTE]
+    
+    # Place the legend outside the plot
+    ax.legend(handles=patches_list, loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.tight_layout()
+    plt.show()
+
+
+# Initializing and running
+grid_dim = [15, 15, 15]
 grid_size = 1.0
 center = np.array([17.773, 63.285, 121.743])
 file_path = "./3c9t.pdb"
 
-
-# Criação de uma instância PDBVoxel
 voxel_instance = PDBVoxel(None, np.zeros(grid_dim), None, center, grid_size)
 parsed_pdb = voxel_instance.parse_pdb(file_path)
 voxel_instance.pdb_to_voxel_atom(parsed_pdb)
-projection_sum = voxel_instance.project_sum()
-
-plt.figure(figsize=(6, 6))
-plt.title('Sum Projection')
-plt.imshow(projection_sum, cmap='viridis')
-plt.colorbar(label='Total Atoms')
-plt.show()
-
-
+property_grid = pdb_to_voxel_property(parsed_pdb, voxel_instance)
+projection_sum, property_projection = project_sum_with_property(voxel_instance, property_grid)
+plot_projection_with_property(projection_sum, property_projection)
