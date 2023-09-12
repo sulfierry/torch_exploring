a41d4dbcd5c1fd0529cce39d07572d054bf71a87
sulfierry <leon.bio0@hotmail.com>
2023-09-12 10:36:38 -0300

    vox

--- vox.py
+++ vox.py
@@ -35,7 +35,7 @@ class PDBVoxel:
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
         plt.show()
-
+    # OK
     def parse_pdb(self, pdb_file_path):
         with open(pdb_file_path, 'r') as file:
             pdb_data = file.readlines()
@@ -66,14 +66,14 @@ class PDBVoxel:
                 pass
 
         return parsed_data
-    
+    # OK
     def coord_to_voxel(self, coord):
         voxel_coord = np.floor((coord - self.center) / self.grid_size).astype(int)
         if np.any(voxel_coord < 0) or np.any(voxel_coord >= self.voxel_grid.shape):
             print(f"Invalid voxel coordinates: {voxel_coord} for atom coordinate: {coord}")
             return None
         return voxel_coord
-
+    # OK
     def pdb_to_voxel_atom(self, parsed_pdb):
         atom_info_grid = np.empty(self.voxel_grid.shape, dtype=object)
         
@@ -85,30 +85,6 @@ class PDBVoxel:
                     atom_info_grid[tuple(voxel_coord)] = atom_details
 
         return self.voxel_grid, atom_info_grid
-
-    def pdb_to_voxel_residue(self, parsed_pdb):
-        residue_info_grid = np.empty(self.voxel_grid.shape, dtype=object)
-
-        seen_residues = set()
-
-        for atom_section in ["chains", "cofactors", "ligands"]:
-            for atom_details in parsed_pdb[atom_section]:
-                residue_identifier = (atom_details['chain_id'], atom_details['res_seq'], atom_details['icode'])
-                if residue_identifier not in seen_residues:
-                    seen_residues.add(residue_identifier)
-                    if atom_details['name'] == 'CA':
-                        voxel_coord = self.coord_to_voxel(np.array(atom_details["coord"]))
-                        if voxel_coord is not None:
-                            self.voxel_grid[tuple(voxel_coord)] = 1
-                            residue_info = {
-                                'res_name': atom_details['res_name'],
-                                'chain_id': atom_details['chain_id'],
-                                'res_seq': atom_details['res_seq'],
-                                'icode': atom_details['icode']
-                            }
-                            residue_info_grid[tuple(voxel_coord)] = residue_info
-
-        return self.voxel_grid, residue_info_grid
 
     def project_sum(self, axis=2):
         if axis not in [0, 1, 2]:
@@ -123,7 +99,7 @@ def get_amino_acid_property(res_name):
             return property
     return None
 
-
+# OK
 def pdb_to_voxel_property(parsed_pdb, voxel_instance):
     property_grid = np.empty(voxel_instance.voxel_grid.shape, dtype=object)
     for atom_section in ["chains", "cofactors", "ligands"]:
@@ -134,7 +110,7 @@ def pdb_to_voxel_property(parsed_pdb, voxel_instance):
                 if aa_property:
                     property_grid[tuple(voxel_coord)] = aa_property
     return property_grid
-
+# OK
 def pdb_to_voxel_amino_acid(parsed_pdb, voxel_instance):
     aa_grid = np.empty(voxel_instance.voxel_grid.shape, dtype=object)
     for atom_section in ["chains", "cofactors", "ligands"]:
@@ -144,6 +120,7 @@ def pdb_to_voxel_amino_acid(parsed_pdb, voxel_instance):
                 aa_grid[tuple(voxel_coord)] = atom_details['res_name']
     return aa_grid
 
+# OK
 def project_sum_with_property_and_aa(voxel_instance, property_grid, aa_grid, axis=2):
     projection_sum = np.sum(voxel_instance.voxel_grid, axis=axis)
     property_projection = np.empty(projection_sum.shape, dtype=object)
@@ -174,44 +151,6 @@ def project_sum_with_property_and_aa(voxel_instance, property_grid, aa_grid, axi
                 aa_projection[x, y] = predominant_aa
 
     return projection_sum, property_projection, aa_projection
-
-
-def project_sum_with_property(voxel_instance, property_grid, axis=2):
-    projection_sum = np.sum(voxel_instance.voxel_grid, axis=axis)
-    property_projection = np.empty(projection_sum.shape, dtype=object)
-    for x in range(projection_sum.shape[0]):
-        for y in range(projection_sum.shape[1]):
-            if axis == 0:
-                properties_in_column = property_grid[x, y, :]
-            elif axis == 1:
-                properties_in_column = property_grid[x, :, y]
-            else:
-                properties_in_column = property_grid[:, x, y]
-            properties_in_column = [prop for prop in properties_in_column if prop]
-            if properties_in_column:
-                unique, counts = np.unique(properties_in_column, return_counts=True)
-                predominant_property = unique[np.argmax(counts)]
-                property_projection[x, y] = predominant_property
-    return projection_sum, property_projection
-
-
-def plot_projection_with_property(projection_sum, property_projection):
-    fig, ax = plt.subplots(figsize=(10, 10))
-    colored_projection = np.zeros(projection_sum.shape + (3,))
-    for x in range(projection_sum.shape[0]):
-        for y in range(projection_sum.shape[1]):
-            if property_projection[x, y]:
-                color = matplotlib.colors.hex2color(PDBVoxel.COLOR_PALETTE[property_projection[x, y]])
-                intensity = projection_sum[x, y] / projection_sum.max()
-                colored_projection[x, y] = [c * intensity for c in color]
-    ax.imshow(colored_projection)
-    ax.set_title('Sum Projection with Amino Acid Properties')
-    patches_list = [patches.Patch(color=PDBVoxel.COLOR_PALETTE[prop], label=prop) for prop in PDBVoxel.COLOR_PALETTE]
-    
-    # Place the legend outside the plot
-    ax.legend(handles=patches_list, loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.tight_layout()
-    plt.show()
 
 
 def project_sum_with_amino_acid(voxel_instance, aa_grid, axis=2):
@@ -265,7 +204,7 @@ def plot_projection_with_corrected_labels(projection_sum, aa_projection, propert
     plt.show()
 
 
-
+# OK
 def plot_projection_by_property_with_labels_adjusted(projection_sum, aa_projection, target_property):
     """Plot the projection filtered by a specific amino acid property with amino acid labels and borders, 
     ensuring even low intensity squares are visible."""
@@ -299,7 +238,7 @@ def plot_projection_by_property_with_labels_adjusted(projection_sum, aa_projecti
 
 
 # Initializing and running
-grid_dim = [10, 10, 10]
+grid_dim = [15, 15, 15]
 grid_size = 1.0
 center = np.array([17.773, 63.285, 121.743])
 file_path = "./3c9t.pdb"
